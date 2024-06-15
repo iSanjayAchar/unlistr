e5ee6973086eeb37d377bc05818213340ebb1643
Sanjay Achar <mr.sanjayachar@gmail.com>
2024-06-15 16:57:59 +0530

    Imntegrating tailwind

--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import './index.scss';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <>Hello, world</>
   </React.StrictMode>
 );
 
