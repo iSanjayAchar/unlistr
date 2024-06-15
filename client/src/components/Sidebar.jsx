082ef6e8be4c8de98877194284252cc15653acf4
Sanjay Achar <mr.sanjayachar@gmail.com>
2024-06-15 20:18:51 +0530

    a bit work on sidebar

---
+++ client/src/components/Sidebar.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+
+function Sidebar() {
+    return (
+        <div className="border-r-2 border-black bg-[#95D2B3]">
+            <div className="flex flex-col h-full">
+                <div className="p-10 rounded-[20px]">
+                    <div className="flex gap-2 items-start">
+                        <img src="/assets/icon.svg" alt="Unlistr" className="w-[48px] h-[48px] mt-[3px]" />
+                        <h1 className="text-left font-bold text-2xl mb-12">
+                            Unlistr.
+                            <small className="block text-sm opacity-80">
+                                The only todo app you need
+                            </small>
+                        </h1>
+                    </div>
+                    <div className="flex flex-col justify-center gap-5">
+                        <div className="flex justify-center gap-5">
+                            <input
+                                type="text"
+                                className="border-2 border-black rounded-none text-3xl placeholder:text-xl px-5 py-3 focus:border-transparent !outline-none active:border-2 active:border-black focus:border-2 focus:border-black focus:shadow-[5px_5px_0px_0px_#000] transition ease-in w-full"
+                                placeholder="What's gotta get done?"
+                            />
+                        </div>
+                        <textarea
+                            placeholder="Details, details! What makes this task tick?"
+                            rows={10}
+                            data-enable-grammarly="false"
+                            className="border-2 border-black rounded-none px-2 py-3 font-medium !outline-none active:border-2 active:border-black focus:border-2 focus:border-black focus:shadow-[5px_5px_0px_0px_#000] transition ease-in w-full"
+                        />
+
+                        <button className="bg-white border-2 border-black px-5 py-5 font-bold hover:shadow-[5px_5px_0px_0px_#000] transition ease-in">
+                            CREATE
+                        </button>
+                    </div>
+                </div>
+                <div className="mt-auto py-5 p-10 flex cursor-pointer gap-4 border-t-2 border-black">
+                    <img src="https://api.dicebear.com/8.x/big-smile/svg?seed=Molly" alt="Profile" className="w-[48px] h-[48px] bg-white rounded-[50%] p-1" />
+                    <h1 className="text-2xl font-bold">
+                        Hola!
+                        <small className="block text-sm -mt-[3px] text-gray-700">Let's sign you up!</small>
+                    </h1>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Sidebar;
